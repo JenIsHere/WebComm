86941efa8c130849709ee902298fad7ff6ec7f98
Jenish Shah <91283665+JenIsHere@users.noreply.github.com>
2022-04-17 17:47:28 -0400

    Add files via upload

--- JS/geojson.js
+++ JS/geojson.js
@@ -1,1 +1,63 @@
-
+var contestedAreas = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [43.860853, -79.772975]
+            },  
+            "type" : "Feature"          
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [43.77503, -80.070094]
+            },           
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [44.40131, -80.539501]
+            },
+            "type": "Feature",
+            "properties": {"popupContent": ""},
+            "id": 3
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [44.046453, -79.538502]
+            },
+            "type": "Feature",
+            "properties": {"popupContent": ""},
+            "id": 4
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [44.102488, -79.478975]
+            },
+            "type": "Feature",
+            "properties": {"popupContent": ""},
+            "id": 5
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [44.284664, -79.570783]
+            },
+            "type": "Feature",
+            "properties": {"popupContent": ""},
+            "id": 6
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [43.996386, -79.17557]
+            },
+            "type": "Feature",
+            "properties": {"popupContent": ""},
+            "id": 7
+        }
+    ]
+};
